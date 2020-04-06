c5f18e04d4f955a69d212f83e615dce23c9652c2
Geonju Park <pkju98@naver.com>
2020-04-06 23:03:09 +0900

    Updates

---
+++ precache-manifest.2708fad332ffd4beb2772155e019aabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b4e64121983e834adae21c05ac245c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e7d22b236be0d5e19d6c",
+    "url": "/movie_app_2020/static/css/main.836a7f50.chunk.css"
+  },
+  {
+    "revision": "d81438a4d598e83349fe",
+    "url": "/movie_app_2020/static/js/2.8d5eb2b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8d5eb2b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d22b236be0d5e19d6c",
+    "url": "/movie_app_2020/static/js/main.0ad715d0.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
